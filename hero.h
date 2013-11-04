07328af4fd115455267e363b62ad0a2d5ed7d751
ProsserCJ <ProsserCJ1@gcc.edu>
2013-11-03 20:20:59 -0500

    Create hero.h

---
+++ hero.h
@@ -0,0 +1,32 @@
+/****************************************************
+Hero header file, containts the definition
+of a hero class
+
+Last modified: 11/3/2013
+
+******************************************************/
+
+#ifndef _HERO_H
+#define _HERO_H
+
+#include "entity.h"
+
+
+class Hero: public Entity{
+public:
+	void attack(DIR);
+	void move(DIR);
+
+	virtual void draw();	
+	virtual void update();	
+	virtual void act();
+
+	
+	DIR getDirectionFacing(){return facing;}
+
+private:
+	DIR facing;
+
+};
+
+#endif
