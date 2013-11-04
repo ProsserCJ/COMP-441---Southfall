b7441a271323c65c495ad63aed4e2aacf7253392
ProsserCJ <ProsserCJ1@gcc.edu>
2013-11-03 20:20:39 -0500

    Create hero.cpp

---
+++ hero.cpp
@@ -0,0 +1,37 @@
+/****************************************************
+Hero implementation file
+
+Last modified: 11/3/2013
+
+******************************************************/
+
+#include "hero.h"
+
+void Hero::attack(DIR d){
+	switch(d){
+	case UP:
+	case DOWN:
+	case LEFT:
+	case RIGHT:
+	case UP_RIGHT:
+	case UP_LEFT:
+	case DOWN_RIGHT:
+	case DOWN_LEFT:
+	}
+
+}	
+
+
+void Hero::move(DIR d){
+	switch(d){
+	case UP:
+	case DOWN:
+	case LEFT:
+	case RIGHT:
+	case UP_RIGHT:
+	case UP_LEFT:
+	case DOWN_RIGHT:
+	case DOWN_LEFT:
+	}
+
+}
